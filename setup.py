7bc8fce34455dbe61a4f0cd7793bab7795935e44
JF Cote <jcote071@gmail.com>
2022-03-09 18:00:27 -0500

    Upload project directory to Github

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Processing and analysis of the baseline survey data collected in Kananga, DRC',
+    author='JF',
+    license='',
+)
